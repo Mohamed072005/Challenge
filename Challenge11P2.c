1e14849ddb37d0dde0ee7e7a33bc1966a79316d7
Mohamed072005 <amine072005@gmail.com>
2023-09-17 15:45:21 +0100

    Challenge C part 2

--- Challenge11P2.c
+++ Challenge11P2.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int main()
+{
+
+    srand(time(0));
+    int a = (rand()% 7)+ 1;
+
+    switch(a)
+    {
+    case 1:
+        printf("Monday");
+        break;
+    case 2:
+        printf("Tuesday");
+        break;
+    case 3:
+        printf("Wednesday");
+        break;
+    case 4:
+        printf("Thursday");
+        break;
+    case 5:
+        printf("Friday");
+        break;
+    case 6:
+        printf("Saturday");
+        break;
+    case 7:
+        printf("Sunday");
+        break;
+
+    }
+    return ;
+
+
+
+}
